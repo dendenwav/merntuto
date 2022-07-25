b58d449a4b6375af00026aed66a5e0b0d7e4c902
Raphaël Coutaud <raphael320coutaud@hotmail.com>
2022-07-25 13:49:15 +0200

    setup form compo

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit'
-import { applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
@@ -10,6 +10,7 @@ import reducers from './reducers';
 import App from './App';
 
 const store = configureStore({reducer: reducers});
+//const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
