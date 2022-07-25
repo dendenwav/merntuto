b5e7783572a047978321e49638f64c8d28921e55
Raphaël Coutaud <raphael320coutaud@hotmail.com>
2022-07-25 18:00:27 +0200

    setup posts and post compo

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 import App from './App';
+import './style.css';
 
 const store = configureStore({reducer: reducers});
 //const store = createStore(reducers, compose(applyMiddleware(thunk)));
